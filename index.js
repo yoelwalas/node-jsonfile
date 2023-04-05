61fcb6b97cf390ae3e51995b190bc1b543b3120b
Sandra Humanista <sandrahumanista@Sandras-iMac.local>
2023-04-05 18:27:59 -0300

    write es igual a writeFileSync

--- index.js
+++ index.js
@@ -1,88 +1,90 @@
-let _fs
+let _fs;
 try {
-  _fs = require('graceful-fs')
+  _fs = require("graceful-fs");
 } catch (_) {
-  _fs = require('fs')
+  _fs = require("fs");
 }
-const universalify = require('universalify')
-const { stringify, stripBom } = require('./utils')
+const universalify = require("universalify");
+const { stringify, stripBom } = require("./utils");
+const { write } = require("fs");
 
-async function _readFile (file, options = {}) {
-  if (typeof options === 'string') {
-    options = { encoding: options }
+async function _readFile(file, options = {}) {
+  if (typeof options === "string") {
+    options = { encoding: options };
   }
 
-  const fs = options.fs || _fs
+  const fs = options.fs || _fs;
 
-  const shouldThrow = 'throws' in options ? options.throws : true
+  const shouldThrow = "throws" in options ? options.throws : true;
 
-  let data = await universalify.fromCallback(fs.readFile)(file, options)
+  let data = await universalify.fromCallback(fs.readFile)(file, options);
 
-  data = stripBom(data)
+  data = stripBom(data);
 
-  let obj
+  let obj;
   try {
-    obj = JSON.parse(data, options ? options.reviver : null)
+    obj = JSON.parse(data, options ? options.reviver : null);
   } catch (err) {
     if (shouldThrow) {
-      err.message = `${file}: ${err.message}`
-      throw err
+      err.message = `${file}: ${err.message}`;
+      throw err;
     } else {
-      return null
+      return null;
     }
   }
 
-  return obj
+  return obj;
 }
 
-const readFile = universalify.fromPromise(_readFile)
+const readFile = universalify.fromPromise(_readFile);
 
-function readFileSync (file, options = {}) {
-  if (typeof options === 'string') {
-    options = { encoding: options }
+function readFileSync(file, options = {}) {
+  if (typeof options === "string") {
+    options = { encoding: options };
   }
 
-  const fs = options.fs || _fs
+  const fs = options.fs || _fs;
 
-  const shouldThrow = 'throws' in options ? options.throws : true
+  const shouldThrow = "throws" in options ? options.throws : true;
 
   try {
-    let content = fs.readFileSync(file, options)
-    content = stripBom(content)
-    return JSON.parse(content, options.reviver)
+    let content = fs.readFileSync(file, options);
+    content = stripBom(content);
+    return JSON.parse(content, options.reviver);
   } catch (err) {
     if (shouldThrow) {
-      err.message = `${file}: ${err.message}`
-      throw err
+      err.message = `${file}: ${err.message}`;
+      throw err;
     } else {
-      return null
+      return null;
     }
   }
 }
 
-async function _writeFile (file, obj, options = {}) {
-  const fs = options.fs || _fs
+async function _writeFile(file, obj, options = {}) {
+  const fs = options.fs || _fs;
 
-  const str = stringify(obj, options)
+  const str = stringify(obj, options);
 
-  await universalify.fromCallback(fs.writeFile)(file, str, options)
+  await universalify.fromCallback(fs.writeFile)(file, str, options);
 }
 
-const writeFile = universalify.fromPromise(_writeFile)
+const writeFile = universalify.fromPromise(_writeFile);
 
-function writeFileSync (file, obj, options = {}) {
-  const fs = options.fs || _fs
+function writeFileSync(file, obj, options = {}) {
+  const fs = options.fs || _fs;
 
-  const str = stringify(obj, options)
+  const str = stringify(obj, options);
   // not sure if fs.writeFileSync returns anything, but just in case
-  return fs.writeFileSync(file, str, options)
+  return fs.writeFileSync(file, str, options);
 }
 
 const jsonfile = {
   readFile,
   readFileSync,
   writeFile,
-  writeFileSync
-}
+  writeFileSync,
+  write: writeFileSync,
+};
 
-module.exports = jsonfile
+module.exports = jsonfile;
